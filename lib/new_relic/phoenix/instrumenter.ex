94217c6d1e4e33f425d6b7c8b9d6880f180ae80a
Vince Foley <vincefoley@gmail.com>
2018-10-15 08:40:02 -0700

    use plug_name

--- lib/new_relic/phoenix/instrumenter.ex
+++ lib/new_relic/phoenix/instrumenter.ex
@@ -15,7 +15,7 @@ defmodule NewRelic.Phoenix.Instrumenter do
 
   def phoenix_error_render(:stop, _time_diff, {conn, status, error}) do
     [
-      default_name: NewRelic.Transaction.Plug.default_name(conn),
+      plug_name: NewRelic.Transaction.Plug.plug_name(conn),
       status: status
     ]
     |> NewRelic.add_attributes()
