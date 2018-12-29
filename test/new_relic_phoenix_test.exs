61a68b334fc53c6a5e6547193d82c15065bf3199
Vince Foley <vincefoley@gmail.com>
2018-12-29 15:02:02 -0800

    Basic test

--- test/new_relic_phoenix_test.exs
+++ test/new_relic_phoenix_test.exs
@@ -1,7 +1,23 @@
 defmodule NewRelicPhoenixTest do
   use ExUnit.Case
 
+  defmodule TestEndpoint do
+    use Phoenix.Endpoint, otp_app: :new_relic_phoenix
+
+    plug(:hello_world)
+
+    defp hello_world(conn, _) do
+      Plug.Conn.send_resp(conn, 200, "Hello, World")
+    end
+  end
+
+  @port 8882
   test "greets the world" do
-    assert true
+    Application.put_env(:new_relic_phoenix, TestEndpoint, http: [port: @port], server: true)
+    TestEndpoint.start_link()
+
+    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/")
+
+    assert body == "Hello, World"
   end
 end
