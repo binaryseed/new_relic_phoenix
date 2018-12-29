2f73b05102ce92330e266a9bffc2e78d3eee9754
Vince Foley <vincefoley@gmail.com>
2018-12-29 15:42:52 -0800

    Flesh out app and assert on events

--- test/new_relic_phoenix_test.exs
+++ test/new_relic_phoenix_test.exs
@@ -3,42 +3,69 @@ defmodule NewRelicPhoenixTest do
 
   alias NewRelic.Harvest.Collector
 
-  # Wire up and instrument our Endpoint
+  # Wire up and instrument our Phoenix App
+  @port 8882
+  Application.put_env(:new_relic_phoenix, __MODULE__.TestEndpoint,
+    http: [port: @port],
+    server: true,
+    instrumenters: [NewRelic.Phoenix.Instrumenter]
+  )
+
+  defmodule TestController do
+    use Phoenix.Controller
+    def index(conn, _params), do: text(conn, "Hello, World")
+    def error(_conn, _params), do: raise("BAD")
+  end
+
+  defmodule ErrorView do
+    def render(_, _), do: "Error"
+  end
+
+  defmodule TestRouter do
+    use Phoenix.Router
+    get "/", TestController, :index
+    get "/error", TestController, :error
+  end
+
   defmodule TestEndpoint do
     use Phoenix.Endpoint, otp_app: :new_relic_phoenix
     use NewRelic.Phoenix.Transaction
-
-    plug(:hello_world)
-
-    defp hello_world(conn, _) do
-      Plug.Conn.send_resp(conn, 200, "Hello, World")
-    end
+    plug TestRouter
   end
 
   # Start up our HTTP Server
-  @port 8882
   setup_all do
-    Application.put_env(:new_relic_phoenix, TestEndpoint,
-      http: [port: @port],
-      server: true,
-      instrumenters: [NewRelic.Phoenix.Instrumenter]
-    )
-
-    start_supervised(TestEndpoint)
+    TestEndpoint.start_link()
     :ok
   end
 
+  @tag :capture_log
   test "Report expected events!" do
     restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
 
-    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/path")
+    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/")
 
     assert body == "Hello, World"
 
     [[_, tx_event]] = gather_harvest(Collector.TransactionEvent.Harvester)
 
     assert tx_event[:status] == 200
-    assert tx_event[:path] == "/path"
+    assert tx_event[:path] == "/"
+    refute tx_event[:error]
+    assert tx_event[:framework_name] == "/Phoenix/NewRelicPhoenixTest.TestController/index"
+    assert tx_event[:"phoenix.controller"] == "NewRelicPhoenixTest.TestController"
+
+    restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+
+    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/error")
+
+    assert body == "Error"
+
+    [[_, tx_event]] = gather_harvest(Collector.TransactionEvent.Harvester)
+
+    assert tx_event[:status] == 500
+    assert tx_event[:path] == "/error"
+    assert tx_event[:error] == true
   end
 
   defp restart_harvest_cycle(harvest_cycle) do
