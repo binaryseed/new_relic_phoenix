556ce81efa559393f35c8a1bd9096877a111cf3c
Vince Foley <vincefoley@gmail.com>
2018-12-29 15:12:56 -0800

    basic plug testing

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,5 @@
 use Mix.Config
+
+if Mix.env() == :test do
+  config :phoenix, :json_library, Jason
+end
--- test/new_relic_phoenix_test.exs
+++ test/new_relic_phoenix_test.exs
@@ -1,8 +1,12 @@
 defmodule NewRelicPhoenixTest do
   use ExUnit.Case
 
+  alias NewRelic.Harvest.Collector
+
+  # Wire up and instrument our Endpoint
   defmodule TestEndpoint do
     use Phoenix.Endpoint, otp_app: :new_relic_phoenix
+    use NewRelic.Phoenix.Transaction
 
     plug(:hello_world)
 
@@ -11,13 +15,38 @@ defmodule NewRelicPhoenixTest do
     end
   end
 
+  # Start up our HTTP Server
   @port 8882
-  test "greets the world" do
-    Application.put_env(:new_relic_phoenix, TestEndpoint, http: [port: @port], server: true)
-    TestEndpoint.start_link()
+  setup_all do
+    Application.put_env(:new_relic_phoenix, TestEndpoint,
+      http: [port: @port],
+      server: true,
+      instrumenters: [NewRelic.Phoenix.Instrumenter]
+    )
 
-    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/")
+    start_supervised(TestEndpoint)
+    :ok
+  end
+
+  test "Report expected events!" do
+    restart_harvest_cycle(Collector.TransactionEvent.HarvestCycle)
+
+    %{body: body} = HTTPoison.get!("http://localhost:#{@port}/path")
 
     assert body == "Hello, World"
+
+    [[_, tx_event]] = gather_harvest(Collector.TransactionEvent.Harvester)
+
+    assert tx_event[:status] == 200
+    assert tx_event[:path] == "/path"
+  end
+
+  defp restart_harvest_cycle(harvest_cycle) do
+    GenServer.call(harvest_cycle, :restart)
+  end
+
+  defp gather_harvest(harvester) do
+    Process.sleep(300)
+    harvester.gather_harvest
   end
 end
