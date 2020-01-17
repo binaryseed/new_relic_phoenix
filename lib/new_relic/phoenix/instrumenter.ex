ac9ab7b7307dddf36685b88c6a0e70b5e7e0808c
Adam Keys <ajkeys@users.noreply.github.com>
2020-01-17 09:59:19 -0600

    Fix formatting

--- lib/new_relic/phoenix/instrumenter.ex
+++ lib/new_relic/phoenix/instrumenter.ex
@@ -16,6 +16,7 @@ defmodule NewRelic.Phoenix.Instrumenter do
   def phoenix_error_render(:stop, _time_diff, {conn, status, error}) do
     NewRelic.add_attributes(status: status)
     NewRelic.Phoenix.Transaction.Plug.before_send(conn)
+
     if status >= 500 do
       NewRelic.Transaction.Reporter.fail(error)
     end
